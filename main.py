a224a681391094c4d30f42a54a0259620d0c5bca
temp_12 <111221831+temp1291@users.noreply.github.com>
2024-08-05 15:14:29 +0300

    Input validation has been fixed

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
         start_number = input('Please enter the starting number: ')
         end_number = input('Please enter the ending number: ')
 
-        if start_number.isdigit() and end_number.isdigit:
+        if start_number.isdigit() and end_number.isdigit():
             print(f'Generating a random number between {start_number} and {end_number}:',
                   f'{randint(int(start_number), int(end_number))}\n')
         else:
